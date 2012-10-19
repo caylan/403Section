b58c8a63422d75088b1a0c9de672cf30884b504c
Andrew Davies <andavies@cs.washington.edu>
2012-10-18 17:54:56 -0700

    Removed class.  Simply a sorting method now.

--- heapsort.py
+++ heapsort.py
@@ -1,13 +1,8 @@
 from heapq import *
 
-class Sorter():
-  def sort(self, lst):
-    pass
-
-class HeapSort(Sorter):
-  def sort(self, lst):
-    heap = list(lst)
-    heapify(heap)
-    for i in range(len(lst)):
-      lst[i] = heappop(heap)
-    return lst
+def sort(self, lst):
+  heap = list(lst)
+  heapify(heap)
+  for i in range(len(lst)):
+    lst[i] = heappop(heap)
+  return lst
