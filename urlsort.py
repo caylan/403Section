f690af631005f33ea83b7a826e95b11a4a537a71
Brian-Oliphant <oliphb@uw.edu>
2012-10-23 18:05:31 -0700

    Adding dummy list generator + sorted list tester

--- urlsort.py
+++ urlsort.py
@@ -9,6 +9,28 @@ Usage: python urlsort.py [filename] [sorting algorithm: (1-4)]
 USAGE = 'Usage: python urlsort.py [filename] [sorting algorithm: (1-4)]'
 
 import sys
+import string
+import random
+
+def url_generator(size=20, chars=string.ascii_lowercase + string.digits):
+  return ''.join(random.choice(chars) for x in range(size))
+
+def list_make():
+        thelist = []
+        for x in range(5):
+                thelist.append(url_generator())
+        thefile = open("output.txt", 'w')
+        for item in thelist:
+                thefile.write("www.%s.com\n" % item)
+
+def test_list():
+        f = open("output.txt", 'r')
+        list_sorted = read_file(f)
+        working = "working"
+        for x in range(len(list_sorted)-1):
+                if list_sorted[x] > list_sorted[x+1]:
+                        working = "not working"
+        print(working)
 
 def read_file(f):
   urls = []
