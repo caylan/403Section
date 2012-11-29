2be4dbf96c8a07ff3b220c6b8d21c3018005c9dc
Andrew Davies <andavies@cs.washington.edu>
2012-11-28 20:38:35 -0800

    Some more comments.

--- url_validation.py
+++ url_validation.py
@@ -1,5 +1,7 @@
 import re
 
+# REGEX ----------------------------------------------------------------------- 
+#
 # The supreme regex, courtesy of Diego Perini.
 # 
 # This attempts to factor in unicode characters as well as ASCII.
@@ -7,9 +9,16 @@ import re
 #
 # http://mathiasbynens.be/demo/url-regex
 #
+# The regex conforms to RFC 3986 (with unicode!) which can be read here:
+# http://www.ietf.org/rfc/rfc3986.txt
+#
+# In which a path is defined as scheme://netloc/path;parameters?query#fragment
+# in a varying number of subsections.  For this regex, however, the matching
+# URLs will be limited to http(s) and ftp
+#
 __regex = u"^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$"
 
-__regex_c = re.compile(__regex, re.U)
+__regex_c = re.compile(__regex, re.U)  # Compile the regex in Unicode format.
 
 def is_valid_url(url):
     return bool(re.match(__regex_c, url))
