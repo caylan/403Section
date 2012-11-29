a979d36447f1c5d4c145c8e9797d116106c72b5a
Andrew Davies <andavies@cs.washington.edu>
2012-11-28 20:18:25 -0800

    Fixed python2/3 compatibility issues.

--- url_validator_tests.py
+++ url_validator_tests.py
@@ -6,13 +6,12 @@ class TestValidUrls(unittest.TestCase):
 
     def setUp(self):
         f = codecs.open('./urls_valid.txt', encoding='utf-8', mode='r')
-        self.URLS = [unicode(line.strip()) for line in f.readlines()]
+        self.URLS = [line.strip() for line in f.readlines()]
         f.close()
 
     def test_all_valid(self):
         for url in self.URLS:
-            print(url)
-            self.assertTrue(validate(url))
+            self.assertTrue(validate(url), msg=u"Failed URL: {0}".format(url))
 
 if __name__ == '__main__':
     unittest.main()
